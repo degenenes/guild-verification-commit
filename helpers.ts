c20b5df020bc6f0895d6c71d2ca619beb731b72a
degenenes <degenenes@gmail.com>
2026-03-04 19:08:27 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-04T16:08:25.538Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-04T16:08:27.189Z
+Commit: Fix memory leak in event handlers
