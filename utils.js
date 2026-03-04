9c8cc287f01cf218b5e2a68bab584ae08f9f2218
degenenes <degenenes@gmail.com>
2026-03-04 19:08:58 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-04T16:08:43.819Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-04T16:08:57.940Z
+Commit: Resolve null pointer exception in user service
